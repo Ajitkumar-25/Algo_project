5eac335c66d7c92161f2b01ddb169163846e1b69
Monic-19 <monicvyas20@gmail.com>
2023-04-28 02:09:11 +0530

    css mostly done

--- script.js
+++ script.js
@@ -101,8 +101,8 @@ function display(e) {
    let len=input.length*8;
    let save=len-ans;
   document.getElementById("string5").innerHTML = 
-    "Total space consumed before encoding : "+ len+"bits\n\n"
-    + "Total space consumed after encoding : "+ans+ "bits\n\n"
+    "Total space consumed before encoding : "+ len+" bits\n\n"
+    + "Total space consumed after encoding : "+ans+ " bits\n\n"
     + "Space saved : "+ save+" bits\n\n";
 }
 
